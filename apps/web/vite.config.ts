d6bda68ce3ea44994c74624df5c2014b7c464f8f
Yanislav Igonin <yanislavdabest@gmail.com>
2026-04-13 23:39:45 +0800

    shadcn stuff

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,16 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		port: 5173,
 	},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
